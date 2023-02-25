7c07b7a41e5110fc10e942624bfb00bc363b41d2
Brayanbs99 <bryanbs2399@gmail.com>
2023-02-24 18:31:45 -0600

    Segundo Commit

--- react-native-clock/App.js
+++ react-native-clock/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { React, useState } from "react";
+import Clock from "./components/Clock";
+import { StyleSheet, View, Button, StatusBar } from "react-native";
 
-export default class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {on: false};
-  }
-  render() {
+const App = () =>{
+    const [on, setOn]= useState(false);
+    const toggleClock = () => {
+        setOn(!on);
+    };
     return (
-      <View style={this.styles.container}>
-        {this.state.on && <Clock/>}
-        <Button 
-          title={`Turn clock ${this.state.on? 'OFF': 'ON'}`}
-          onPress= {()=> {this.setState({on: !this.state.on})}}
-          />
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
+        <View style={styles.container}>
+          {on && <Clock/>}
+          <Button 
+            title={`Turn clock ${on? 'OFF': 'ON'}`}
+            onPress= {toggleClock}
+            />
+          <StatusBar style="auto" />
+        </View>
+      );
+}
 
-  styles = StyleSheet.create({
+styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
@@ -28,46 +27,5 @@ export default class App extends Component{
       justifyContent: 'center',
     },
   });
-}
 
-class Clock extends Component {
-  tick = 0;
-
-  constructor(props) {
-    console.log('constructor called')
-    super(props);
-    this.state = {
-      time: '00:00',
-    };
-  }
-  
-  render(){
-    console.log('Render called');
-    return(
-      <View>
-          <Text>
-              {this.state.time}
-          </Text>
-      </View>
-    );
-  }
-
-  componentDidMount(){
-    console.log('Esta es una prueba');
-    this.tick = setInterval(()=>{
-      this.setState({
-        time: new Date().toLocaleTimeString(),
-      });
-      console.info('tick');
-    },1000);
-  }
-
-  componentDidUpdate (){
-    console.log('Hola');
-  }
-
-  componentWillUnmount() {
-    console.log('componentWillUnmount called');
-    clearInterval(this.tick);
-  }
-} 
+export default App;
--- react-native-clock/components/Clock.jsx
+++ react-native-clock/components/Clock.jsx
@@ -1,8 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { View, Text } from "react-native";
 
 export default function Clock(){
     let [time, setTime] = useState('00:00');
-    let tick = 0
+
+    useEffect(() => {
+        let tick = setInterval(() => {
+            setTime(new Date().toLocaleTimeString());
+            console.info('tick');
+        }, 1000);
+        return () => {
+            clearInterval(tick);
+        };
+    }, []); //Este arreglo hace que se llame en todos los renders, sin este, se ejecuta solo una vez
+
+    useEffect(() => {
+        console.log('rendering');
+    });
 
     return(
       <View>
